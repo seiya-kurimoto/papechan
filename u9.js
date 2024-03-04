8d971012eee5964e08973e2271d2fa6a32d039b4
SuperKojima <hayato.kojima@steamship.co.jp>
2024-03-04 14:56:45 +0900

    問題追加

---
+++ u9.js
@@ -0,0 +1,22 @@
+const arr = [
+    {
+        rank: 'B',
+        score: 20,
+    },
+    {
+        rank: 'A',
+        score: 25,
+    },
+    {
+        rank: 'A',
+        score: 24,
+    },
+]
+
+/*
+[
+    { rank: 'A', age: 25 },
+    { rank: 'A', age: 24 },
+    { rank: 'B', age: 20 }
+]
+*/
