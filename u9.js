b7d6bb2ae7f73c6c4710d6d6a44c519dcb755d6f
SuperKojima <hayato.kojima@steamship.co.jp>
2024-03-06 10:31:34 +0900

    fic

--- u9.js
+++ u9.js
@@ -9,6 +9,10 @@ const arr = [
     },
     {
         rank: 'A',
+        score: 23,
+    },
+    {
+        rank: 'A',
         score: 24,
     },
 ]
@@ -17,6 +21,7 @@ const arr = [
 [
     { rank: 'A', age: 25 },
     { rank: 'A', age: 24 },
+    { rank: 'A', age: 23 },
     { rank: 'B', age: 20 }
 ]
 */
