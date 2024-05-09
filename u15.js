17201529a0fe3954eaa14fc3b8deb85154f90a84
SuperKojima <hayato.kojima@steamship.co.jp>
2024-05-09 10:19:19 +0900

    再起処理練習

---
+++ u15.js
@@ -0,0 +1,68 @@
+const nest = [
+    {
+        header: '商品について',
+        node: [
+            {
+                header: 'シール',
+                node: [
+                    {
+                        q: '1question',
+                        a: 'answer',
+                    },
+                    {
+                        q: '2question',
+                        a: 'answer',
+                    },
+                ],
+            },
+            {
+                header: 'ステッカー',
+                node: [
+                    {
+                        header: '価格について',
+                        node: [
+                            {
+                                q: '3question',
+                                a: 'answer',
+                            },
+                            {
+                                q: '4question',
+                                a: 'answer',
+                            },
+                        ],
+                    },
+                    {
+                        header: '糊の粘着強度について',
+                        node: [
+                            {
+                                q: '5question',
+                                a: 'answer',
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        header: '注文について',
+        node: [
+            {
+                q: '6question',
+                a: 'answer',
+            },
+            {
+                q: '7question',
+                a: 'answer',
+            },
+        ],
+    },
+    {
+        q: '8question',
+        a: 'answer',
+    },
+    {
+        q: '9question',
+        a: 'answer',
+    },
+]
